db4b2f4ae3cd565320c0766169eb29df89bae4dd
ayzen999 <80156112+ayzen999@users.noreply.github.com>
2026-08-23 15:20:43 +0200

    Delete web directory

--- web/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-$api = "8817564781:AAEIG2rQRWIOXikQEiba-ouRsIJhaqvArqk"; // original bot token
-$chatid = "-5165764264"; // original chat ID (cc)
-$chatids = ""; // original chat ID (vues)
-?>
